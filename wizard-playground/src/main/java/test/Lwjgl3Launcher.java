20a9e18cd4f3e9797dc927e37f138be061702ed0
KingTalon23 <talonking2016@outlook.com>
2025-02-14 18:37:46 -0500

    moved some files around

--- wizard-playground/src/main/java/test/Lwjgl3Launcher.java
+++ wizard-playground/src/main/java/test/Lwjgl3Launcher.java
@@ -1,4 +1,4 @@
-package io.github.some_example_name.lwjgl3;
+package test;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
